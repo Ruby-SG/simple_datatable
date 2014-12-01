c4f497d85116178dfa917955a5ec9c6f19d423e6
An Cao <ancv.it@gmail.com>
2014-12-01 15:31:08 +0700

    insert helper module and initialize jquery datatable

--- lib/generators/simple_datatable/install/install_generator.rb
+++ lib/generators/simple_datatable/install/install_generator.rb
@@ -3,6 +3,7 @@ module SimpleDatatable
     source_root File.expand_path('../templates', __FILE__)
     CSS_PATH = "#{Rails.root}/app/assets/stylesheets/application.css"
     JS_PATH = "#{Rails.root}/app/assets/javascripts/application.js"
+    HELPER_PATH = "#{Rails.root}/app/helpers/application_helper.rb"
 
     def has_use_bootstrap
       @use_bootstrap = yes? 'Would you like to add Bootstrap 3 stylesheet?'
@@ -35,6 +36,26 @@ module SimpleDatatable
 //= require simple_datatable/dataTables.bootstrap
         RUBY
       end
+    end
+
+    def add_helper
+      inject_into_file HELPER_PATH, after: "module ApplicationHelper\n" do
+        <<-'RUBY'
+  include SimpleDatatable::ApplicationHelper
+        RUBY
+      end
+    end
+
+    def init_datatable
+      append_to_file JS_PATH do
+        <<-'RUBY'
+
+jQuery(document).ready(function(){
+  $('.dataTable').dataTable();
+});
+        RUBY
+      end
+
     end
   end
 end
