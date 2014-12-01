207720dc50058e67ffeb86357d9abcc243bd7e70
An Cao <ancv.it@gmail.com>
2014-12-01 15:31:22 +0700

    create helper for simple_datatable

--- app/helpers/simple_datatable/application_helper.rb
+++ app/helpers/simple_datatable/application_helper.rb
@@ -1,4 +1,18 @@
 module SimpleDatatable
   module ApplicationHelper
+    def simple_datatable(id: nil, klass: '', url: '', cols: [])
+      content_tag :table, id: id, class: klass + ' dataTable', 'data-url' => url do
+        content_tag :thead do
+          content_tag :tr do
+            col_html = ""
+            cols.each do |col|
+              col.capitalize!
+              col_html << content_tag(:th, col)
+            end
+            col_html.html_safe
+          end
+        end
+      end.html_safe
+    end
   end
 end
